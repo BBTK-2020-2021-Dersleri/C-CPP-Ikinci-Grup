f320ae52470151fdae5991e5a0489db90291ab29
Muhammed Talha Şahin <56067408+M-TalhaSahin@users.noreply.github.com>
2020-12-05 17:40:17 +0300

    Ders-4

---
+++ 05-12-2020-Ders-4/01.c
@@ -0,0 +1,66 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+
+int sumof3(int, int, int);
+double average3(int, int, int);
+int powerof(int, int);
+void count(int);
+int toplama(int);
+
+int main() {
+
+	
+	printf("%d\n", sumof3(3, 4, 4)); // dönüş değeri void olmayan fonksiyonları değişkenler gibi kullanabiliriz
+	printf("%lf\n", average3(3, 4, 4));
+	printf("%d\n", powerof(sumof3(1,1,2), 2));// uygun return tipine sahip fonksiyonları parametre olarak gönderebiliriz
+
+	//-----------------------------------
+	for (int i = 5; i > 0; i--)
+	{
+		printf("%d\t", i);
+	}
+	printf("\n");
+	// üstteki for döngüsünün yaptığı işi recursion mantığı ile count() fonksiyonunda yazdık
+	count(5);// return değeri void olduğu için herhangi bir atama yapmadık, 
+	// çağırdığımızda fonksiyonun kendi içindeki print() fonksiyonlarından output aldık
+	//-----------------------------------
+	printf("\n%d\n", toplama(4));
+
+	system("pause");
+}
+
+int sumof3(int a, int b, int c) {
+	
+	return a + b + c; // (a + b + c) değerini fonksiyonun çağırıldığı yere döndürdük
+}
+
+double average3(int a, int b, int c)//3 sayının ortalaması
+{
+	return (double)sumof3(a, b, c)/3; // sumof3() fonksiyonunu burda kullanarak toplama işlemini ona yaptırdık
+} 
+
+int powerof(int a, int pow) { // üst alma foknsiyonu
+	int sonuc = 1; // üstüne çarparak ekleyeceğimiz için başlangıç değerimizi 1 yaptık
+	for (int i = 0; i < pow; i++) // girilen üst kadar döndürdük
+	{
+		sonuc = sonuc * a;
+	}
+	return sonuc;
+}
+
+void count(int a) // girilen değerden 1 e kadar eksilterek sayan fonksiyon
+{
+	if (a == 0)
+		return;
+	printf("%d\t", a);
+	count(a - 1);
+	printf("%d", a);
+}
+
+int toplama(int a) // girilen değerin ve altındaki bütün tam sayıların toplamı (0 a kadar)
+{
+	if (a == 0)
+		return 0;
+	int sonuc = a + toplama(a - 1);// 4 + toplama(3) x 3 + toplama(2) x 2 + toplama(1) x 1 + toplama(0)
+	return sonuc;
+}
---
+++ 05-12-2020-Ders-4/02.c
@@ -0,0 +1,65 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int sumof3(int, int, int);
+double average3(int, int, int);
+void notprint(long long int, int, int, int);
+bool is_valid(long long int, int, int, int);
+
+int main() {
+
+	notprint(152120181071, 105, 55, 20);
+	notprint(152120181042, 80, 56, 60);
+
+	system("pause");
+}
+
+void notprint(long long int id, int mat, int fizik, int spor) {
+	float ortalama = average3(mat, fizik, spor);
+	printf("*********************************************\n");
+	if (is_valid(id, mat, fizik, spor)) // eğer inputlarım geçerli ise
+	{
+		printf("%lld numarali ogrencinin yil sonu notları:\n", id);
+		printf("Matematik: %d\n", mat);
+		printf("Fizik:     %d\n", fizik);
+		printf("Spor:      %d\n", spor);
+		printf("          -------\n");
+		printf("Ortalama:  %lf\n", ortalama);
+		if (ortalama >= 50) {
+			printf("Gecti.\n");
+		}
+		else {
+			printf("Gecemedi!\n");
+		}
+	}
+	else 
+	{
+		printf("Yanlis input!\n");
+	}
+
+	printf("*********************************************\n");
+
+}
+
+bool is_valid(long long int id, int mat, int fizik, int spor) {
+	if (mat < 0 || mat > 100)
+		return 0;
+	if (fizik < 0 || fizik > 100)
+		return 0;
+	if (spor < 0 || spor > 100)
+		return 0;
+	if (id < 100000000000 || id >= 1000000000000)
+		return 0;
+	return 1;
+}
+
+int sumof3(int a, int b, int c) {
+
+	return a + b + c;
+}
+
+double average3(int a, int b, int c) {
+
+	return (double)sumof3(a, b, c) / 3;
+}
